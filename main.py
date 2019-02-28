3985ff5cc50018fe2078133c15eea1060558075f
Kristof Van Loock <kristof.vanloock@skynet.be>
2019-02-28 19:23:47 +0100

    score function

--- main.py
+++ main.py
@@ -15,3 +15,11 @@ for pic in data:
     pics.append(d)
     id += 1
 
+
+def score(Img1, Img2):
+    identical = 0
+    for tag in Img1["tags"]:
+        for tag2 in Img2["tags"]:
+            if tag == tag2:
+                identical += 1
+    return min(identical, Img1["nb_tags"] - identical, Img2["nb_tags"] - identical)
