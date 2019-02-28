e0d903f06ac0fffed01d09fdb04d49dc07f5d6d5
Kristof Van Loock <kristof.vanloock@skynet.be>
2019-02-28 19:57:41 +0100

    Total score

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import numpy as np
+from itertools import permutations
+
 
 f = open('a_example.txt', 'r')
 
 N = int(f.readline().strip('\n'))
+print(N)
 
 data = [[char for char in line.strip('\n').split(' ')] for line in f.readlines()]
 
@@ -11,7 +14,7 @@ pics = []
 id = 0
 
 for pic in data:
-    d = {'id': id, 'o': pic[0], 'nb_tags': int(pic[1]), 'tags': pic[2:]}
+    d = {'id': id, 'o': pic[0], 'nb_tags': int(pic[1]), 'tags': set(pic[2:])}
     pics.append(d)
     id += 1
 
@@ -23,3 +26,14 @@ def score(Img1, Img2):
             if tag == tag2:
                 identical += 1
     return min(identical, Img1["nb_tags"] - identical, Img2["nb_tags"] - identical)
+
+
+def total_score(photos):
+    t_score = 0
+    for i in range(len(photos) - 1):
+        t_score += score(photos[i], photos[i+1])
+    return t_score
+
+
+all_combos = list(permutations(pics))
+
