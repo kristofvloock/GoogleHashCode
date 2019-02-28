7e4a487eb0eb6c9bea163bb8bb26f10bc912c41a
Kristof Van Loock <kristof.vanloock@skynet.be>
2019-02-28 19:21:53 +0100

    Import testset

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world!")
+import numpy as np
+
+f = open('a_example.txt', 'r')
+
+N = int(f.readline().strip('\n'))
+
+data = [[char for char in line.strip('\n').split(' ')] for line in f.readlines()]
+
+pics = []
+
+id = 0
+
+for pic in data:
+    d = {'id': id, 'o': pic[0], 'nb_tags': int(pic[1]), 'tags': pic[2:]}
+    pics.append(d)
+    id += 1
+
