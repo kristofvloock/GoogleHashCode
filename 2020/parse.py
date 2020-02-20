32a852735c83f09ff47452262ae4e88fa5dd6fc8
Kristof Van Loock <kristof.vanloock@skynet.be>
2020-02-20 19:25:06 +0100

    parser done

--- 2020/parse.py
+++ 2020/parse.py
@@ -1,6 +1,20 @@
-filename = 'a_example.txt'
+from math import ceil
+filename = 'b_read_on.txt'
 
 f = open(filename, 'r')
-B, L, D = f.readline().strip('\n').split(' ')
+B, L, D = [int(i) for i in f.readline().strip('\n').split(' ')]
+book_scores = [int(i) for i in f.readline().strip('\n').split(' ')]
 
-print(header)
+libs = [] ###################
+for l in range(L):
+        nob, sup, rate =  [int(i) for i in f.readline().strip('\n').split(' ')]
+        books = [int(i) for i in f.readline().strip('\n').split(' ')]
+        scan_days = ceil(nob/rate)
+        lib = {
+            'nob': nob,
+            'sup': sup,
+            'rate': rate,
+            'books': books,
+            'scan_days': int(ceil(nob/rate))
+        }
+        libs.append(lib)
